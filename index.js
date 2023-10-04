96b3e436b25bf7ef61adf71071d012d7ebe91cc0
AntonioMereu <antn.mereu@gmail.com>
2023-10-04 15:52:38 +0200

    complete js functionality

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const submit = document.querySelector('button');
 
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener('change', function () {
-        if (this.checked) {
-            handleCheckboxChange(+this.id);
+
+        for (let i = 1; i <= 5; i++) {
+            if (i !== +this.id) {
+                const checkedValue = document.getElementById(i);
+                checkedValue.checked = false;
+            }
+        }
+        document.querySelector('.rating-value').textContent = `${this.id} `;
+    });
+
+    submit.addEventListener('click', () => {
+        if (!checkbox.checked) {
+            console.log('No rating selected!');
+        } else {
+            document.querySelector('.rating-card').style.display = 'none';
+            document.querySelector('.thanks-card').style.display = 'flex';
         }
     });
 });
-
-function handleCheckboxChange(checkboxId) {
-    for (let i = 1; i <= 5; i++) {
-        if (i !== checkboxId) {
-            document.getElementById(i).checked = false;
-        }
-    }
-}
