f5717cbadb8e728ba54a06fcb96d528534daafa8
AntonioMereu <antn.mereu@gmail.com>
2023-10-04 16:39:29 +0200

    add mediaQ for desktop

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const submit = document.querySelector('button');
 
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener('change', function () {
-
         for (let i = 1; i <= 5; i++) {
             if (i !== +this.id) {
                 const checkedValue = document.getElementById(i);
@@ -11,11 +10,18 @@ checkboxes.forEach(checkbox => {
             }
         }
         document.querySelector('.rating-value').textContent = `${this.id} `;
+        document.querySelector('.warning').style.display = 'none';
+        document.querySelectorAll('.rating span').forEach(span => {
+            span.style.border = 'none';
+        });
     });
 
     submit.addEventListener('click', () => {
         if (!checkbox.checked) {
-            console.log('No rating selected!');
+            document.querySelector('.warning').style.display = 'block';
+            document.querySelectorAll('.rating span').forEach(span => {
+                span.style.border = 'solid 1px var(--warning)';
+            });
         } else {
             document.querySelector('.rating-card').style.display = 'none';
             document.querySelector('.thanks-card').style.display = 'flex';
